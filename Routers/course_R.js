2b4c6f2c7fc108e90d373ee7251caa7f3966fc44
Tks23-hub <forcodin23@gmail.com>
2025-04-02 19:06:10 +0300

    Starting to create CRUD for Courses Table using Middleware and Routes

---
+++ Routers/course_R.js
@@ -0,0 +1,6 @@
+const express = require("express");
+const router = express.Router();
+module.exports = router;
+const course_Mid = require("../middleware/course_Mid");
+
+router.post("Add", [course_Mid.AddCourse], (req, res) => {});
---
+++ middleware/course_Mid.js
@@ -0,0 +1,16 @@
+async function AddCourse(req, res, next) {
+  let name = addSlashes(req.body.name); // a' or (drop table courses --
+  let Query = `INSERT INTO courses( name) VALUES ('${name}')`;
+  const promisePool = db_pool.promise();
+  let rows = [];
+  try {
+    [rows] = await promisePool.query(Query);
+  } catch (err) {
+    console.log(err);
+  }
+  next();
+}
+
+module.exports = {
+  AddCourse,
+};
